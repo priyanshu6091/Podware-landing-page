2801c43581ebc2226381c8eab557c297b8b8d0e3
priyanshu6091 <145902260+priyanshu6091@users.noreply.github.com>
2024-12-14 17:46:36 +0530

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { User, Podcaster, EarlyAccessRequest } from '../types';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api'
+  baseURL: 'https://podware-landing-page.onrender.com/api'
 });
 
 export const earlyAccessService = {
